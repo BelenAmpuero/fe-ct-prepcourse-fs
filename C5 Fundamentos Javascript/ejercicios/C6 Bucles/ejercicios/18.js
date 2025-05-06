6b6470fa1bc7a6a93c68515187bf146fe2ad1b1b
BelenAmpuero <bnampuero@gmail.com>
2025-05-06 18:43:42 -0300

    Ejercicios C6

---
+++ C5 Fundamentos Javascript/ejercicios/C6 Bucles/ejercicios/18.js
@@ -0,0 +1,16 @@
+function productoEntreNúmeros(a, b) {
+  // Dados dos argumentos "a" y "b", devuelve el producto de todos
+  // los números entre a y b (inclusive).
+  // Tu código:
+  let producto = 1;
+  if (a > b) {
+    [a, b] = [b, a];
+  }
+  for (let i = a; i <= b; i++) {
+    producto *= i;  }
+  
+    return producto;
+  }
+
+
+module.exports = productoEntreNúmeros;
